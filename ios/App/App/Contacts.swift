34180fa8f3683ac92c63d15e597dcbea23681082
carlpoole <mail@carlpoole.com>
2020-04-15 02:20:26 -0500

    iOS comments

--- ios/App/App/Contacts.swift
+++ ios/App/App/Contacts.swift
@@ -4,7 +4,18 @@ import Contacts
 
 @objc(Contacts)
 public class Contacts : CAPPlugin {
+    
+    /**
+    Contact properties that can be searched.
+     */
+    let contactDetailsMap : Set<String> = ["name", "firstName", "lastName", "phone", "email" ]
 
+    /**
+     Retrieves all contacts on the device.
+     
+     - Parameters:
+        - call: The plugin call to report back to
+     */
     @objc func getAll(_ call: CAPPluginCall) {
         
         // Fetch and respond with contacts from off the main UI thread
@@ -15,10 +26,28 @@ public class Contacts : CAPPlugin {
         }
     }
     
+    /**
+     Finds specific contacts on the device.
+     
+     - Parameters:
+        - call: The plugin call to report back to
+     */
     @objc func find(_ call: CAPPluginCall) {
         // Get the search property and value from the plugin call
         let searchProperty = call.getString("property") ?? ""
         let searchValue = call.getString("value") ?? ""
+        
+        // Make sure the property is a valid option currently supported by the plugin.
+        if (!contactDetailsMap.contains(searchProperty)) {
+            call.error(String(format: "Unrecognized contact search property: %s", searchProperty))
+            return
+        }
+        
+        // Shortcut empty result if no search value passed.
+        if (searchValue.isEmpty) {
+            call.success()
+            return
+        }
         
         // Fetch and respond with contacts from off the main UI thread
         DispatchQueue.global(qos: .userInitiated).async {
@@ -28,6 +57,11 @@ public class Contacts : CAPPlugin {
         }
     }
     
+    /**
+     Fetches the entire list of contacts on the device.
+     
+     - Returns: The list of contacts on the device
+     */
     private func fetchContacts() -> [Any] {
         let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey, CNContactEmailAddressesKey]
         let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
@@ -46,6 +80,15 @@ public class Contacts : CAPPlugin {
         return contacts
     }
     
+    /**
+     Fetches contacts provided a search property and value and returns filtered results.
+     
+     - Parameters:
+        - property: The contact property to search on, eg: name, or email
+        - value: The value to search contacts with
+     
+     - Returns: A list of contacts filtered by the provided search parameters
+     */
     private func fetchContacts2(property: String, value: String) -> [Any] {
         let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey, CNContactEmailAddressesKey] as [CNKeyDescriptor]
         let predicate = CNContact.predicateForContacts(matchingName: "Apple")
@@ -65,6 +108,14 @@ public class Contacts : CAPPlugin {
         return results
     }
     
+    /**
+     Helper function for constructing a response object from the plugin when provided an iOS Contact object
+     
+     - Parameters:
+        - contact: The contact to format
+     
+     - Returns: A contact to return from the plugin
+     */
     private func buildContactResponse(contact: CNContact) -> Any {
         var phones = [Any]()
         
